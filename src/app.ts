1ff966583c27f77d252afc98e7805945de664417
Seif Sheikhelarab <seifusama@gmail.com>
2025-11-24 00:50:31 +0200

    update cors

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN?.split(", ") || [],
+    origin: process.env.CORS_ORIGIN?.split(",") || [],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
